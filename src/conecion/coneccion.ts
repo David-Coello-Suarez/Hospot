1ea3d4d193ce89e87bebdf268499d8cf378f01cc
David Coello S <coellosuarezd@gmail.com>
2021-12-10 23:56:07 -0500

    Config DB

--- src/conecion/coneccion.ts
+++ src/conecion/coneccion.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize"
 
 const conexion: Sequelize = new Sequelize(`mysql://uqmzksitwdgvg4nw:mnCevtzJqBy1ybSWCQnI@bhw1zvw36q2zxmctgely-mysql.services.clever-cloud.com:3306/bhw1zvw36q2zxmctgely`, {
     dialect: 'mysql',
-    logging: true,
+    logging: false,
     // query: {
     //     raw: true,
     // },
