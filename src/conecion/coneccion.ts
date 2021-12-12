ab5d68e261209118f795648c8fc08ed02ead2b8b
David Coello S <coellosuarezd@gmail.com>
2021-12-11 22:52:19 -0500

    DB local

--- src/conecion/coneccion.ts
+++ src/conecion/coneccion.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize"
 
-const conexion: Sequelize = new Sequelize(`mysql://udmblwbci2efnadz:hib3C0d5ZBxw2yW2QWwv@brstnivzvrvn55x5coqi-mysql.services.clever-cloud.com:3306/brstnivzvrvn55x5coqi`, {
+const conexion: Sequelize = new Sequelize(`mysql://davidc:Davidc.1@localhost:3306/hotspot`, {
     dialect: 'mysql',
     logging: false,
     // query: {
