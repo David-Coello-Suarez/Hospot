7b768e1f237664ef051d574a74ec71b12151fb0c
David Coello S <coellosaurezd@gmail.com>
2022-01-02 18:44:07 -0500

    unificando BK Y DB

--- src/conecion/coneccion.ts
+++ src/conecion/coneccion.ts
@@ -1,6 +1,11 @@
 import { Sequelize } from "sequelize"
 
-const conexion: Sequelize = new Sequelize(`mysql://davidc:Davidc.1@@localhost/hotspot`, {
+import * as dotenv from "dotenv"
+
+dotenv.config()
+console.log("Server DB",process.env.DB)
+
+const conexion: Sequelize = new Sequelize(`${process.env.BD}`, {
     dialect: "mysql",
     logging: false,
     // query: {
--- src/controllers/CUsuario.ts
+++ src/controllers/CUsuario.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { Usuario } from "../models/usuario"
+import MikroNode from "mikronode-ng2"
 
 const GetUsuarios = (req: Request, res: Response) => {
     return res.status(200).json({
@@ -16,6 +17,27 @@ const PostUsuario = async (req: Request, res: Response) => {
     // await registrarUsuario.save()
 
     if (registrarUsuario) {
+        var connection = MikroNode.getConnection(`${process.env.HOSTMIKTO}`,`${process.env.USERMIKRO}`,`${process.env.PASSMIKRO}`)
+        connection.connect(function (conn: { openChannel: () => any; closeOnDone: boolean }) {
+            var chan = conn.openChannel()
+            conn.closeOnDone = true
+            chan.write("/ip/address/print", function () {
+                chan.closeOnDone = true
+                chan.on("done", function (data: any) {
+                    var parsed = MikroNode.parseItems(data)
+                    parsed.forEach(function (item: { interface: string; address: string }) {
+                        console.log("Interface/IP: " + item.interface + "/" + item.address)
+                    })
+                })
+                chan.once("trap", function (trap: string, chan: any) {
+                    console.log("Command failed: " + trap)
+                })
+                chan.once("error", function (err: string, chan: any) {
+                    console.log("Oops: " + err)
+                })
+            })
+        })
+
         return res.status(200).json({
             estado: 1,
             msj: "Usuario Creado",
